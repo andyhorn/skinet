08126c441cb09ffc6c5762f8851bc36adda0c5aa
Andy Horn <andyjhorn@gmail.com>
2024-03-28 21:55:46 -0700

    Add Product repo and interface

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+  public class ProductRepository(StoreContext _context) : IProductRepository
+  {
+    public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+    {
+      return await _context.ProductBrands.ToListAsync();
+    }
+
+    public async Task<Product> GetProductByIdAsync(int id)
+    {
+      return await _context.Products.FindAsync(id);
+    }
+
+    public async Task<IReadOnlyList<Product>> GetProductsAsync()
+    {
+      return await _context.Products.ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+    {
+      return await _context.ProductTypes.ToListAsync();
+    }
+  }
+}
