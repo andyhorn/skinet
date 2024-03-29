bae733ed1a2ef3dde854b31e061bf0d550ac29eb
Andy Horn <andyjhorn@gmail.com>
2024-03-28 21:56:07 -0700

    Update ProductsController with repository

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,20 +8,20 @@ namespace API.Controllers
 {
   [ApiController]
   [Route("api/[controller]")]
-  public class ProductsController(StoreContext context) : ControllerBase
+  public class ProductsController(IProductRepository _productRepository) : ControllerBase
   {
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetProducts()
     {
-      var products = await context.Products.ToListAsync();
-      return products;
+      var products = await _productRepository.GetProductsAsync();
+      return Ok(products);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-      var product = await context.Products.FindAsync(id);
-      return product;
+      var product = await _productRepository.GetProductByIdAsync(id);
+      return Ok(product);
     }
   }
 }
