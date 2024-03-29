fee629e91c01b9c0102d189b8e14387333e505de
Andy Horn <andyjhorn@gmail.com>
2024-03-29 07:34:00 -0700

    Use generic repo in controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -8,33 +8,37 @@ namespace API.Controllers
 {
   [ApiController]
   [Route("api/[controller]")]
-  public class ProductsController(IProductRepository _productRepository) : ControllerBase
+  public class ProductsController(
+    IGenericRepository<Product> _productRepository,
+    IGenericRepository<ProductType> _productTypeRepository,
+    IGenericRepository<ProductBrand> _productBrandRepository
+  ) : ControllerBase
   {
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetProducts()
     {
-      var products = await _productRepository.GetProductsAsync();
+      var products = await _productRepository.GetAllAsync();
       return Ok(products);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-      var product = await _productRepository.GetProductByIdAsync(id);
+      var product = await _productRepository.GetByIdAsync(id);
       return Ok(product);
     }
 
     [HttpGet("brands")]
     public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
     {
-      var brands = await _productRepository.GetProductBrandsAsync();
+      var brands = await _productBrandRepository.GetAllAsync();
       return Ok(brands);
     }
 
     [HttpGet("types")]
     public async Task<ActionResult<List<ProductType>>> GetProductTypes()
     {
-      var types = await _productRepository.GetProductTypesAsync();
+      var types = await _productTypeRepository.GetAllAsync();
       return Ok(types);
     }
   }
