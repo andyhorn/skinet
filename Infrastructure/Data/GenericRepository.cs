89d8e25bf222ff0d68ed74fc314d9c448e7c1236
Andy Horn <andyjhorn@gmail.com>
2024-03-29 07:58:12 -0700

    Add get and list to generic repo

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -5,7 +5,9 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data
 {
@@ -19,6 +21,21 @@ namespace Infrastructure.Data
     public async Task<T> GetByIdAsync(int id)
     {
       return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task<T> GetEntityWithSpec(ISpecification<T> specification)
+    {
+      return await ApplySpecification(specification).FirstOrDefaultAsync();
+    }
+
+    public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> specification)
+    {
+      return await ApplySpecification(specification).ToListAsync();
+    }
+
+    private IQueryable<T> ApplySpecification(ISpecification<T> specification)
+    {
+      return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), specification);
     }
   }
 }
